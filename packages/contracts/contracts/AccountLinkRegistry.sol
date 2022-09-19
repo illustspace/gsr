e32b164065103368f32356856f93d9d519c63005
Will Ockelmann-Wagner <will@illust.space>
2022-09-19 15:24:55 -0700

    remove pausable, ownable, and fix solidty version

--- packages/contracts/contracts/AccountLinkRegistry.sol
+++ packages/contracts/contracts/AccountLinkRegistry.sol
@@ -2,7 +2,7 @@
 /// @author Illust Space
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.4;
 pragma abicoder v2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
