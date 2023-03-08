4dcda6144ae0f176798c7663ba8b1baf465b2437
Will Ockelmann-Wagner <will@illust.space>
2023-03-08 15:31:48 -0800

    bump sdk version and add links

--- packages/indexer/next.config.js
+++ packages/indexer/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["page.tsx", "page.ts", "api.ts"],
+  eslint: {
+    // CI already handles this, so don't do it twice.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
