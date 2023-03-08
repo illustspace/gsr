0f84ce442a297d26867451c2a044d33fc553b6be
Will Ockelmann-Wagner <will@illust.space>
2023-03-08 10:32:43 -0800

    validate envs with yup to get optional infura ids

--- packages/indexer/features/config/validateApiConfig.ts
+++
@@ -1,8 +0,0 @@
-/** Catch missing env variables on start. */
-export const validateApiConfig = (name: string, env: Record<string, any>) => {
-  Object.entries(env).forEach(([key, value]) => {
-    if (value === "" || value === undefined || Number.isNaN(value)) {
-      throw new Error(`missing ${name} env var ${key}`);
-    }
-  });
-};
